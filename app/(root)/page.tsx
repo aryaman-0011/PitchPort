600b1565937aa69b836ad59be43bfb8465f2880a
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-04-08 23:22:35 +0530

    Current date

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -8,9 +9,9 @@ export default async function Home({
   const query = (await searchParams).query;
   const posts = [
     {
-      _createdAt: "Yesterday",
+      _createdAt: new Date(),
       views: 55,
-      author: { _id: 1 },
+      author: { _id: 1, name: 'Ishu' },
       _id: 1,
       description: "This is a description",
       image: "https://images.unsplash.com/photo-1485827404703-89b55fcc595e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -35,7 +36,13 @@ export default async function Home({
         </p>
 
         <ul className="mt-7 card_grid">
-
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startup found</p>
+          )}
         </ul>
       </section>
     </>
