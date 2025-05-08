41bb98b6921e34c556b524bc876340e292fc9227
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-05-09 01:55:02 +0530

    Added view component for dynamic view count

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -5,16 +5,23 @@ import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
-import React from 'react'
+import React, { Suspense } from 'react'
+
+import markdownit from 'markdown-it'
+import { Skeleton } from '@/components/ui/skeleton';
+import View from '@/components/View';
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+
+    const md = markdownit()
 
     const id = (await params).id;
 
     const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
 
-    if (!post) return notFound();
-    
+    if (!post) return notFound()
+
+    const parsedContent = md.render(post?.pitch || '')
 
     return (
         <>
@@ -42,8 +49,27 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                         <p className='category-tag'>{post.category}</p>
                     </div>
                     <h3 className='text-30-bold'>Pitch Details</h3>
+                    {parsedContent ? (
+                        <article className='prose max-w-4xl font-work-sans break-all'
+                            dangerouslySetInnerHTML={{ __html: parsedContent }}
+                        />
+                    ) : (
+                        <p className='no-result'>
+                            No details Provided
+                        </p>
+                    )}
                 </div>
+                <hr className='divider' />
+
+                {/* TODO: EDITOR SELECTED STARTUP */}
+
+                <Suspense fallback={<Skeleton className='view_skeleton' />}>
+                    <View id={id} />
+                </Suspense>
+
             </section>
+
+
         </>
     )
 }
