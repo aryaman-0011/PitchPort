5d8d06f37d5d8d1f1bf19340f4eecd8ef759ce9e
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-05-09 01:53:54 +0530

    Define query to count views

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY =
-    defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
     _id,
     title,
     slug,
@@ -17,7 +17,7 @@ export const STARTUPS_QUERY =
 
 
 export const STARTUP_BY_ID_QUERY =
-    defineQuery(`*[_type == "startup" && _id == $id][0]{
+  defineQuery(`*[_type == "startup" && _id == $id][0]{
   _id, 
   title, 
   slug,
@@ -31,3 +31,9 @@ export const STARTUP_BY_ID_QUERY =
   image,
   pitch,
 }`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0]{
+  _id, views
+  }
+`)
