475b44d5f4ed7c97ef4649d4adb3b2ff94c07793
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-06-29 22:13:59 +0530

    applied ppr

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -11,6 +11,8 @@ import markdownit from 'markdown-it'
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
 
+export const experimental_ppr = true;
+
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
     const md = markdownit()
