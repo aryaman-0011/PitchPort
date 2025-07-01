0e2aec268973c52ac03f9cfa407a8dab41fd5e31
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-07-01 19:22:07 +0530

    Fixed a typo

--- lib/validation.ts
+++ lib/validation.ts
@@ -11,7 +11,7 @@ export const formSchema = z.object({
             try {
                 const res = await fetch(url, { method: 'HEAD' })
                 const contentType = res.headers.get("content-type")
-                return contentType?.startsWith("/image")
+                return contentType?.startsWith("image/")
             } catch {
                 return false;
             }
