c7b8cbbe97a2658796f0fbac1a98eb74bc759bd7
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-07-02 19:13:36 +0530

    Added startup card skeleton loading

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { auth } from "@/auth";
+import { StartupCardSkeleton } from "@/components/StartupCard";
 import UserStartups from "@/components/UserStartups";
 import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
@@ -38,7 +40,9 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                     </p>
 
                     <ul className="card_grid-sm">
-                        <UserStartups id={id} />
+                        <Suspense fallback={<StartupCardSkeleton />}>
+                            <UserStartups id={id} />
+                        </Suspense>
                     </ul>
                 </div>
             </section>
