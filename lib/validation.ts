f6896e02bce57a1b9df2324cfed24a036935fddc
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-07-03 11:47:04 +0530

    Fixed image upload error

--- lib/validation.ts
+++ lib/validation.ts
@@ -4,17 +4,10 @@ export const formSchema = z.object({
     title: z.string().min(3).max(100),
     description: z.string().min(20).max(500),
     category: z.string().min(3).max(20),
-    link: z
-        .string()
-        .url()
-        .refine(async (url) => {
-            try {
-                const res = await fetch(url, { method: 'HEAD' })
-                const contentType = res.headers.get("content-type")
-                return contentType?.startsWith("image/")
-            } catch {
-                return false;
-            }
-        }),
+    link: z.string().url().refine((url) => {
+        return /\.(png|jpe?g|gif|webp|bmp|svg)$/i.test(url);
+    }, {
+        message: "URL must end with a valid image extension (e.g., .jpg, .png)"
+    }),
     pitch: z.string().min(10)
 })
