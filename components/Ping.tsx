2ab42ab6ce8cad3185d637e196ff33db182abf28
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-07-03 12:54:04 +0530

    UI changes

--- components/Ping.tsx
+++ components/Ping.tsx
@@ -5,8 +5,8 @@ const Ping = () => {
         <div className='relative'>
             <div className='absolute -left-4 top-1'>
                 <span className='flex size-[11px]'>
-                    <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-primary opacity-75'></span>
-                    <span className='relative inline-flex size-[11px] rounded-full bg-primary'></span>
+                    <span className='absolute inline-flex h-full w-full animate-ping rounded-full bg-blue-800 opacity-75'></span>
+                    <span className='relative inline-flex size-[11px] rounded-full bg-blue-800'></span>
                 </span>
             </div>
         </div>
