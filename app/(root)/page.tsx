c5131668acec2508d652b2d99fcb8806245bab69
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-07-04 21:27:06 +0530

    Updated tagline

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -22,7 +22,7 @@ export default async function Home({
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">Pitch Your Startup, <br /> Connect with Entrepreneurs</h1>
+        <h1 className="heading">Pitch to Collaborate, <br /> Not Just Compete</h1>
         <p className="sub-heading !max-w-3xl">
           Submit ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
